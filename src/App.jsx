c655df3974c03c12fff0884a7e5e54fbb0328cd6
Rebiya <rebum.19@gmail.com>
2025-01-05 21:54:34 +0300

    api from backend logic is done at utils api.js file

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,37 @@ import Websites from "./Pages/Landing/Websites";
 import Properties from "./Pages/Properties/Properties";
 import Bookings from "./Pages/Bookings/Bookings";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Property from "./Components/Property/Property";
 import AboutUs from "./Pages/AboutUs/AboutUs";
 import AddProperties from "./Components/AddProperties/AddProperties";
-import AdminDashBoard from "./Pages/AdminDashBoard/AdminDashBoard"
+import AdminDashBoard from "./Pages/AdminDashBoard/AdminDashBoard";
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <div className="App">
-        <Suspense fallback={<div>Loading...</div>}>
+    <div className="App">
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <QueryClientProvider client={queryClient}>
           <BrowserRouter>
-            <Routes>
-              {/* Use Layout as the parent route */}
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Websites />} />
-                <Route path="properties" element={<Properties />} />
-                <Route path="/bookings" element={<Bookings />} />
-                <Route path="/AboutUs" element={<AboutUs />} />
-                <Route path="/AddProperties" element={<AddProperties />} />
-                <Route path="/AdminDashBoard" element={<AdminDashBoard />} />
-              </Route>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                {/* Use Layout as the parent route */}
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<Websites />} />
+                  <Route path="properties" element={<Properties />} />
+                  <Route path="/bookings" element={<Bookings />} />
+                  <Route path="/AboutUs" element={<AboutUs />} />
+                  <Route path="/AddProperties" element={<AddProperties />} />
+                  <Route path="/AdminDashBoard" element={<AdminDashBoard />} />
+                </Route>
+              </Routes>
+            </Suspense>
           </BrowserRouter>
-        </Suspense>
-      </div>
-    </MantineProvider>
+        </QueryClientProvider>
+      </MantineProvider>
+    </div>
   );
 }
 
---
+++ src/utils/api.js
@@ -0,0 +1,137 @@
+import axios from "axios";
+import dayjs from "dayjs";
+import { toast } from "react-toastify";
+export const api = axios.create({
+  baseURL: "https://localhost:8000/api"
+});
+
+//to get all properties for the home page
+export const getAllProperties = async () => {
+  try {
+    const response = await api.get("/Property", {
+      timeout: 30 * 1000
+    });
+
+    if (response.status === 400 || response.status === 500) {
+      throw response.data;
+    }
+    return response.data;
+  } catch (error) {
+    toast.error("something went wrong, try again later!");
+    throw error;
+  }
+};
+
+//to get a single property for property detail page
+export const getProperty = async (id) => {
+  try {
+    const response = await api.get(`/Property/${id}`, {
+      timeout: 30 * 1000
+    });
+    if (response.status === 400 || response.status === 500) {
+      throw response.data;
+    }
+    return response.data;
+  } catch (error) {
+    toast.error(
+      "something went wrong while fetching Property, try again later!"
+    );
+    throw error;
+  }
+};
+
+//for login authentication using jwt
+export const createUser = async (username, password, token) => {
+  try {
+    await api.post(
+      `/user/login`,
+      { username, password },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+  } catch (error) {
+    toast.error("Something went wrong while you login, Please try again");
+    throw error;
+  }
+};
+
+//to update the status of the property when a user book it
+export const bookStatus = async (status, id, token) => {
+  try {
+    await api.put(
+      `/Property/${id}/status`,
+      {
+        propertyid: id,
+        status: status
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+  } catch (error) {
+    toast.error("Something went wrong while updating status, Please try again");
+    throw error;
+  }
+};
+
+//used when admin needs to delete booked properties and users want to cancel the booked property
+export const removeBooking = async (id, username, token) => {
+  try {
+    await api.delete(`/Property/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      data: {
+        username
+      }
+    });
+  } catch (error) {
+    toast.error(
+      "Something went wrong while deleting property, Please try again"
+    );
+
+    throw error;
+  }
+};
+
+//used in home page of the admin dashboard
+export const getAllBookings = async (username, token) => {
+  if (!token) return;
+  try {
+    const res = await api.post(
+      `/user/allBookings`,
+      {
+        username
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+    return res.data["bookedVisits"];
+  } catch (error) {
+    toast.error("Something went wrong while fetching bookings");
+    throw error;
+  }
+};
+
+//when admin wants to upload new property
+export const createResidency = async (data, token) => {
+  console.log(data);
+  try {
+    const res = await api.post(`/Property`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } catch (error) {
+    toast.error("Something went wrong while uploading property");
+    throw error;
+  }
+};
